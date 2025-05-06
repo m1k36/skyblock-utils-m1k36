0c87a58a8dc41cd98ded25c49da1c6afa539ca1b
m1k36 <mikepiot479@gmail.com>
2025-05-06 20:28:01 +0200

    Fix mayor timer being wrong

--- src/components/mayorComponent/ElectionTimer.tsx
+++ src/components/mayorComponent/ElectionTimer.tsx
@@ -1,6 +1,12 @@
 'use client'
 import React, {useEffect, useState} from 'react'
-import {COLOR_MAP, formatTimeLeftWithDays, getTimeLeftEndElection, getTimeStartEndElection,} from "@/lib/utils";
+import {
+    COLOR_MAP,
+    CURRENT_DAY, ELECTION_END_SKYBLOCK_DAY, ELECTION_START_SKYBLOCK_DAY,
+    formatTimeLeftWithDays,
+    getTimeLeftEndElection,
+    getTimeStartEndElection,
+} from "@/lib/utils";
 
 export default function ElectionTimer() {
 
@@ -16,12 +22,12 @@ export default function ElectionTimer() {
 
     return (
         <>
-            {timeLeftEndElection < timeLeftStartElection ?
-                <p className="text-lg"><span className={COLOR_MAP['6']}>Time left: </span><span
-                    className={COLOR_MAP['a']}>{formatTimeLeftWithDays(timeLeftEndElection)}</span></p>
-                :
+            {CURRENT_DAY > ELECTION_END_SKYBLOCK_DAY && CURRENT_DAY < ELECTION_START_SKYBLOCK_DAY ?
                 <p className="text-lg"><span className={COLOR_MAP['6']}>Start in: </span><span
                     className={COLOR_MAP['a']}>{formatTimeLeftWithDays(timeLeftStartElection)}</span></p>
+                :
+                <p className="text-lg"><span className={COLOR_MAP['6']}>Time left: </span><span
+                    className={COLOR_MAP['a']}>{formatTimeLeftWithDays(timeLeftEndElection)}</span></p>
             }
         </>
     );
